16afc77a557142ae73f43b5427b83706bbd930e9
Salman Farshe <salmanfarshe39@gmail.com>
2021-02-02 22:26:17 +0600

    making To-Do with styling

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// check off specific todo 
+$("ul").on("click","li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to deleting todo
+$("ul").on("click","span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        // remove total li, after fade-out
+        $(this).remove();
+    });
+    //prevent from event bubbling up
+    event.stopPropagation();
+});
+
+// val() extract the values from input
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){ // Enter key = 13
+        // grab the new todos from input
+        let newTodo = $(this).val();
+        // creat e a new li & add to ul
+        //.append() takes a string of html
+        $("ul").append("<li><span><i class='far fa-trash-alt fa-1.5x'></i></span> " + newTodo + "</li>");
+    }
+});
+
+// creating todo by plus icon
+$(".fa-plus").on("click", function(){
+    $("input[type='text']").fadeToggle();
+});
